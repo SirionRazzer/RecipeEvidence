289ebd28cfbc8b6761ffa88c5fe525e8b78d418c
Tomas Soukal <xsoukal2@fi.muni.cz>
2017-03-10 09:29:58 +0100

    Created packages and classes

---
+++ RecipeEvidence/src/main/java/cz/muni/fi/pv168/recipeevidence/CategoryManager.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pv168.recipeevidence;
+
+/**
+ * TODO: create javadoc you lazy bitch
+ *
+ * @author Tomas Soukal
+ */
+public interface CategoryManager {
+}
---
+++ RecipeEvidence/src/main/java/cz/muni/fi/pv168/recipeevidence/impl/RecipeManagerImpl.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pv168.recipeevidence.impl;
+
+/**
+ * TODO: create javadoc you lazy bitch
+ *
+ * @author Tomas Soukal
+ */
+public class RecipeManagerImpl {
+}

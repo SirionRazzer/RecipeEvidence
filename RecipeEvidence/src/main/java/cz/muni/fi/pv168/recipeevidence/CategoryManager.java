368f211d11674430d15db87af3c19c1c39df5a16
Tomas Soukal <xsoukal2@fi.muni.cz>
2017-03-10 20:03:52 +0100

    Prepared Category, CategoryManager and its interface

--- RecipeEvidence/src/main/java/cz/muni/fi/pv168/recipeevidence/CategoryManager.java
+++ RecipeEvidence/src/main/java/cz/muni/fi/pv168/recipeevidence/CategoryManager.java
@@ -1,9 +1,29 @@
 package cz.muni.fi.pv168.recipeevidence;
 
+import cz.muni.fi.pv168.recipeevidence.impl.Category;
+
+import java.util.List;
+
 /**
- * TODO: create javadoc you lazy bitch
+ * Represents functions for handling categories
  *
  * @author Tomas Soukal
  */
 public interface CategoryManager {
+
+    /**
+     * Stores new category into database.
+     * @param category
+     */
+    void createCategory(Category category);
+
+    void updateCategory(Category category);
+
+    void deleteCategory(Category category);
+
+    Category findCategoryById(Long id);
+
+    List<Category> findCategoryByName(String name);
+
+    List<Category> findAllCategories();
 }
--- RecipeEvidence/src/main/java/cz/muni/fi/pv168/recipeevidence/impl/CategoryManagerImpl.java
+++ RecipeEvidence/src/main/java/cz/muni/fi/pv168/recipeevidence/impl/CategoryManagerImpl.java
@@ -1,9 +1,36 @@
 package cz.muni.fi.pv168.recipeevidence.impl;
 
+import cz.muni.fi.pv168.recipeevidence.CategoryManager;
+
+import java.util.List;
+
 /**
  * TODO: create javadoc you lazy bitch
  *
  * @author Tomas Soukal
  */
-public class CategoryManagerImpl {
+public class CategoryManagerImpl implements CategoryManager {
+    public void createCategory(Category category) {
+
+    }
+
+    public void updateCategory(Category category) {
+
+    }
+
+    public void deleteCategory(Category category) {
+
+    }
+
+    public Category findCategoryById(Long id) {
+        return null;
+    }
+
+    public List<Category> findCategoryByName(String name) {
+        return null;
+    }
+
+    public List<Category> findAllCategories() {
+        return null;
+    }
 }
